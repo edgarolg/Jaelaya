4937622973abab146d6bc8854c9b9f9495876543
Ana <anaestrada.0405@gmail.com>
2025-04-06 07:33:09 -0600

    decor mine and book transition

--- scenes/bookPOV/book_pov.gd
+++ scenes/bookPOV/book_pov.gd
@@ -1,7 +1,7 @@
 extends Node
 
 @export var scene_to_change: PackedScene
-@export var time:int
-func _ready():
-	await get_tree().create_timer(time).timeout
-	get_tree().change_scene_to_packed(scene_to_change)
+
+func _process(_delta):
+	if Input.is_action_just_pressed("click"):  # "Enter" por defecto
+		get_tree().change_scene_to_packed(scene_to_change)
